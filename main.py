f71c84c3ebae2d83ac012b5e18659b3a197c1b25
Léonie <leonie.schmit@esme.fr>
2026-03-10 11:33:36 +0100

    init maain to API URL

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import requests
+ def main():
+    url = "https://catfact.ninja/fact"
+    response = requests.get(url)
+    
+    print("Status code :", response.status_code)
+    print("Response content :", response.text)
+    
+if __name__ == "__main__":
+    main()
